4d72600582b5343a50f7cd7b8ccd27abb57a61a0
JoshuaIsaac-19 <joshuaisaacraj@gmail.com>
2024-05-30 11:41:50 +0530

    chip push

--- src/app/home/page-list/page-list.component.ts
+++ src/app/home/page-list/page-list.component.ts
@@ -11,15 +11,31 @@ export class PageListComponent {
   @Input() actionArray!: any[];
   @Output() actionEmit= new EventEmitter<any>();
 
+  selectedChip: object={};
   constructor(){}
 
   ngOnInit(): void { 
-    console.log('title', this.title)
-    console.log('description', this.description);
-    console.log('actionArray', this.actionArray);
+    // console.log('title', this.title)
+    // console.log('description', this.description);
+    // console.log('actionArray', this.actionArray);
+
+    this.selectedChip= this.actionArray.find(action=>action.value ==='all');
+    console.log(this.selectedChip);
+    if(this.selectedChip) {
+      this.onActionEmit(this.selectedChip);
+    }
   }
   onActionEmit(event: any){
-    console.log('onActionEmit', event);
-    this.actionEmit.emit(event)
+    // console.log('event' ,event)
+    if(this.selectedChip!==event){
+
+      console.log('onActionEmit', event);
+      this.selectedChip=event;
+      this.actionEmit.emit(event)
+    }else {
+      // If the same chip is clicked, emit the event to maintain the selection
+      this.actionEmit.emit(this.selectedChip);
+    }
+   
   }
 }
--- src/app/home/table/table.component.ts
+++ src/app/home/table/table.component.ts
@@ -1,6 +1,5 @@
 import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
